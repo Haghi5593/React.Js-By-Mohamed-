43d1de98531bb0cc1cd6d91d3bf6b73d137b0ee6
Haghi5593 <dundun709400@gmail.com>
2025-10-20 22:41:09 +0300

    Exercise 01

---
+++ Exercise 01/src/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = () => {
+    return(
+        <div>
+            <h2>Dugsiiye CEO </h2>
+            <p>Dugsiiye@example.com</p>
+        </div>
+    )
+}
+
+export default UserCard;
