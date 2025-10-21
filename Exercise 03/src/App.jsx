8b06b16f09d486fee9d333b3537f7fc53ea2ffbc
Haghi5593 <dundun709400@gmail.com>
2025-10-21 16:12:33 +0300

    Exercise 03

---
+++ Exercise 03/src/App.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./UserCard";
+
+function App() {
+  return (
+    <div>
+      <h1>User List</h1>
+
+      <UserCard name="Mohamed Ali" email="mohamed@example.com" />
+      <UserCard name="Daana Noor" email="daana@example.com" />
+      <UserCard name="Ayaan Warsame" email="ayaan@example.com" />
+    </div>
+  );
+}
+
+export default App;
