8c3c4026dbadf49dcc70e217947973b16f4f0a40
Haghi5593 <dundun709400@gmail.com>
2025-11-25 17:06:18 +0300

    Completed Exercise 17

---
+++ Exercise 17/src/App.jsx
@@ -0,0 +1,12 @@
+import ContactForm from './ContactForm'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Using Custom Hook</h1>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default App;
